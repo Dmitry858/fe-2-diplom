816808a8596a76efa2e2fc3a3a4028c39209dd06
Dmitry <frontender@inbox.ru>
2019-07-07 22:06:10 +0400

    Component 'RatingStars' is done

--- frontend/src/RatingStars.js
+++ frontend/src/RatingStars.js
@@ -3,17 +3,50 @@ import React, { Component } from 'react';
 export default class RatingStars extends Component {
   constructor (props) {
     super(props);
-    this.state = {};
+    this.state = {
+      stars: ['fa-star-o', 'fa-star-o', 'fa-star-o', 'fa-star-o', 'fa-star-o'],
+      isRated: false
+    };
+  }
+  
+  mouseEnterHandler(index) {
+    if (this.state.isRated) return;
+    let newArray = this.state.stars.map((item, i) => {
+      if (i <= index) {
+        return 'fa-star';
+      } else {
+        return 'fa-star-o';
+      }
+    });
+
+    this.setState({
+      stars: newArray
+    });
+  }
+  
+  mouseLeaveHandler() {
+    if (this.state.isRated) return;
+    this.setState({
+      stars: ['fa-star-o', 'fa-star-o', 'fa-star-o', 'fa-star-o', 'fa-star-o']
+    });
+  }
+  
+  clickHandler() {
+    this.setState({
+      isRated: true
+    });
   }
   
   render() {
+    const { stars } = this.state;
+    
     return (
       <div className="order-card__rating-stars">
-        <i className="fa fa-star" aria-hidden="true"></i>
-        <i className="fa fa-star" aria-hidden="true"></i>
-        <i className="fa fa-star-o" aria-hidden="true"></i>
-        <i className="fa fa-star-o" aria-hidden="true"></i>
-        <i className="fa fa-star-o" aria-hidden="true"></i>
+        { stars.map((item, i) => {
+          return (
+            <i key={i} className={`fa ${item}`} aria-hidden="true" onMouseEnter={this.mouseEnterHandler.bind(this, i)} onMouseLeave={this.mouseLeaveHandler.bind(this)} onClick={this.clickHandler.bind(this)}></i>
+          );
+        })}
       </div>
     );    
   }
