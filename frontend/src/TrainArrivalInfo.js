0c30af0a3ba0d2c2df788b5c8b12f9573240f6c6
Dmitry <frontender@inbox.ru>
2019-07-13 15:01:02 +0400

    Component 'TrainArrivalInfo' is done

--- frontend/src/TrainArrivalInfo.js
+++ frontend/src/TrainArrivalInfo.js
@@ -5,23 +5,33 @@ export default class TrainArrivalInfo extends Component {
     super(props);
   }
   
+  timeFormatConverter(time) {
+    if (String(time).length === 1) return `0${time}`;
+    return time;
+  }
+  
   render() {
-    if (this.props.item.arrival) {
+    const { item } = this.props;
+    if (item.arrival) {
+      let arrivalFromTime = new Date(item.arrival.from.datetime);
+      let arrivalToTime = new Date(item.arrival.to.datetime);
+      let arrivalDurationTravel = new Date(item.arrival.duration);
+      
       return (
         <div className="train__info train__info_back">
           <div className="train__info_back_departure">
-            <p className="train__info-time">00:10</p>
-            <p className="train__info-city">Москва</p>
-            <p className="train__info-station">Курский вокзал</p>
+            <p className="train__info-time">{ `${this.timeFormatConverter(arrivalFromTime.getHours())}:${this.timeFormatConverter(arrivalFromTime.getMinutes())}` }</p>
+            <p className="train__info-city">{item.arrival.from.city.name[0].toUpperCase() + item.arrival.from.city.name.substring(1)}</p>
+            <p className="train__info-station">{`${item.arrival.from.railway_station_name} вокзал`}</p>
           </div>
           <div className="train__info_back_arrow">
-            <p className="train__travel-time">9 : 42</p>
+            <p className="train__travel-time">{ `${arrivalDurationTravel.getHours()} : ${this.timeFormatConverter(arrivalDurationTravel.getMinutes())}` }</p>
             <i className="fa fa-long-arrow-left" aria-hidden="true"></i>
           </div>
           <div className="train__info_back_arrival">
-            <p className="train__info-time">09:52</p>
-            <p className="train__info-city">Санкт-Петербург</p>
-            <p className="train__info-station">Ладожский вокзал</p>
+            <p className="train__info-time">{ `${this.timeFormatConverter(arrivalToTime.getHours())}:${this.timeFormatConverter(arrivalToTime.getMinutes())}` }</p>
+            <p className="train__info-city">{item.arrival.to.city.name[0].toUpperCase() + item.arrival.to.city.name.substring(1)}</p>
+            <p className="train__info-station">{`${item.arrival.to.railway_station_name} вокзал`}</p>
           </div>
         </div>
       ); 
